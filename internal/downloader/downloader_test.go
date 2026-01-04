19f10d108962b2a15376870246b1a2242d5024bd
Florent Sorel <florent.sorel@gmail.com>
2026-01-04 16:13:33 +0100

    add test

--- internal/downloader/downloader_test.go
+++ internal/downloader/downloader_test.go
@@ -1,8 +1,16 @@
 package downloader
 
 import (
+	"compress/gzip"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
+
+	"github.com/tracker-tv/tmdb-ids-producer/internal/config"
+	"github.com/tracker-tv/tmdb-ids-producer/internal/models"
 )
 
 func TestGetFileName(t *testing.T) {
@@ -15,4 +23,45 @@ func TestGetFileName(t *testing.T) {
 }
 
 func TestDownload(t *testing.T) {
+	testdataFile := "../../testdata/movie_ids_12_30_2025.json"
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data, err := os.ReadFile(testdataFile)
+		if err != nil {
+			t.Fatalf("failed to read testdata file: %v", err)
+		}
+
+		w.Header().Set("Content-Type", "application/gzip")
+		gzipWriter := gzip.NewWriter(w)
+		defer gzipWriter.Close()
+
+		if _, err := gzipWriter.Write(data); err != nil {
+			t.Fatalf("failed to write gzipped response: %v", err)
+		}
+	}))
+	defer server.Close()
+
+	cfg := &config.Config{
+		BaseURL:   server.URL,
+		OutputDir: t.TempDir(),
+		Type:      models.Movie,
+	}
+
+	now := time.Date(2025, time.December, 30, 0, 0, 0, 0, time.UTC)
+	downloader := New(nil, cfg)
+	downloader.Now = now
+
+	filePath, err := downloader.Download()
+	if err != nil {
+		t.Fatalf("Download failed: %v", err)
+	}
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		t.Errorf("Downloaded file does not exist: %s", filePath)
+	}
+
+	expectedFilename := "movie_ids_12_30_2025.json.gz"
+	if filepath.Base(filePath) != expectedFilename {
+		t.Errorf("expected filename %s, got %s", expectedFilename, filepath.Base(filePath))
+	}
 }
